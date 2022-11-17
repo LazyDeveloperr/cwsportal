53c95afee7c1978db05b9062d20577eecea48695
Lazy Developer <107105531+LazyDeveloperr@users.noreply.github.com>
2022-11-17 15:06:17 +0530

    Add files via upload

--- src/Components/Screens/AuthScreens/LoginScreen.js
+++ src/Components/Screens/AuthScreens/LoginScreen.js
@@ -1,30 +1,45 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './Css_Auth.css'
+import axios from 'axios'
+import { Link } from "react-router-dom";
+import Footer from '../SplitScreens/Footer';
+import Header from '../SplitScreens/Header';
 const LoginScreen = () => {
+
+    const [email, setEmail] = useState()
+	const [ password, setPassword] = useState()
+
+    const handleSubmit = (e) => {
+		e.preventDefault()
+		axios.post("http://127.0.0.1:8000/api/login",{email:email, password:password})
+		.then(res=> console.log(res))
+	}
   return (
     <>
     <div className="LazyContainer">
+	<Header/>
        <div className="box">
-		<form autocomplete="off">
+		<form autoComplete="off">
 			<h2>Sign in</h2>
 			<div className="inputBox">
-				<input type="text" required="required"/>
+				<input type="text" required="required" onChange={(e)=>setEmail(e.target.value)}/>
 				<span>Userame</span>
 				<i></i>
 			</div>
 			<div className="inputBox">
-				<input type="password" required="required"/>
+				<input type="password" required="required" onChange={(e)=>setPassword(e.target.value)}/>
 				<span>Password</span>
 				<i></i>
 			</div>
 			<div className="links">
 				<a href="#">Forgot Password ?</a>
-				<a href="#">Signup</a>
+				<Link to="/register">Signup</Link>
 			</div>
-			<input type="submit" value="Login"/>
+			<input onClick={handleSubmit} type="submit" value="Login"/>
 		</form>
 	   </div>
     </div>
+	   <Footer/>
     </>
   )
 }
---
+++ src/Components/Screens/AuthScreens/RegisterScreen.js
@@ -0,0 +1,68 @@
+import React, {useState} from 'react'
+import './Css_Auth.css'
+import axios from 'axios'
+import { Link } from "react-router-dom";
+import Header from '../SplitScreens/Header';
+import Footer from '../SplitScreens/Footer';
+
+const RegisterScreen = () => {
+
+    const [ email, setEmail ] = useState()
+	const [ password, setPassword ] = useState()
+    const [ contact, setContact ] = useState()
+    const [ name, setName ] = useState()
+
+    const handleSubmit = (e) => {
+		e.preventDefault()
+		axios.post("http://127.0.0.1:8000/api/register",{name:name,contact:contact,email:email, password:password})
+		.then(res=> console.log(res))
+	}
+	
+  return (
+    <>
+    <div className="LazyContainerz">
+	 <Header/>
+       <div className="boxx">
+		<form autoComplete="off" className='formz'>
+			<h2>Register</h2>
+			<div className="inputBoxx">
+				<input type="text" required="required" onChange={(e)=>setName(e.target.value)}/>
+				<span>Name</span>
+				<i></i>
+			</div>
+			<div className="inputBoxx">
+				<input type="text" required="required" onChange={(e)=>setContact(e.target.value)}/>
+				<span>Contact</span>
+				<i></i>
+			</div>
+			<div className="inputBoxx">
+				<input type="text" required="required" onChange={(e)=>setEmail(e.target.value)}/>
+				<span>Email</span>
+				<i></i>
+			</div>
+			<div className="inputBoxx">
+				<input type="text" required="required" onChange={(e)=>setPassword(e.target.value)}/>
+				<span>Password</span>
+				<i></i>
+			</div>
+			{/* <input onClick={handleSubmit} className="" type="submit" value="Register"/> */}
+			<div className="btn btn-success py-2 mt-4">Register</div>
+			<div className="div text-white">
+			<div className="flex w-100">
+			<div className="text-center w-full">
+			   <span><small>Already have an account ?</small></span>
+			   <Link to="/" >
+			   <span className='fs-5 px-2' style={{ color:"#45f3ff"}}><small>Login</small></span>
+			   </Link>
+			 </div>	
+			</div>	
+			</div>
+		</form>
+	   </div>
+    </div>
+	<Footer/>
+    </>
+  )
+}
+
+export default RegisterScreen
